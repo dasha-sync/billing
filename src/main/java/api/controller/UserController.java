ecb67ff72a1f940af207b47be82dcc8db60752e3
daria <kapoor.darya@gmail.com>
2025-10-02 18:43:41 +0300

    add user controller

---
+++ src/main/java/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package api.controller;
+
+import api.dto.common.ApiResponse;
+import api.dto.user.*;
+import api.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@PreAuthorize("isAuthenticated()")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @DeleteMapping("/destroy")
+    public ResponseEntity<ApiResponse<Void>> deleteUser(
+            @Valid @RequestBody DeleteUserRequest request,
+            Principal principal,
+            HttpServletResponse response) {
+        userService.deleteUser(request, principal, response);
+        return ResponseEntity.ok(new ApiResponse<>("User deleted successfully", null));
+    }
+}
